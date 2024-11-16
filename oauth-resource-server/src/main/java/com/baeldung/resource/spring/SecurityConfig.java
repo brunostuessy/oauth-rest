fae5e9ab048a5ef1c42852b62a3c036f68be6757
brunostuessy <bruno.stuessi@gmail.com>
2024-11-16 16:38:10 +0100

    Update devfile in RS

--- oauth-resource-server/src/main/java/com/baeldung/resource/spring/SecurityConfig.java
+++ oauth-resource-server/src/main/java/com/baeldung/resource/spring/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,15 +13,16 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.oauth2.core.authorization.OAuth2AuthorizationManagers.hasScope;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-            .cors(AbstractHttpConfigurer::disable)
+            //.cors(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> {
-                auth.requestMatchers("/user/info", "/api/foos/**").access(hasScope("message:read"));
-                auth.requestMatchers("/api/foos").access(hasScope("message:write"));
+                auth.requestMatchers("/user/info", "/api/foos/**").permitAll(); //.access(hasScope("message:read"));
+                auth.requestMatchers("/api/foos").permitAll(); //.access(hasScope("message:write"));
                 //auth.requestMatchers("/error/**").permitAll();
                 //auth.requestMatchers("/api/auth/**").permitAll();
                 auth.anyRequest().authenticated();
